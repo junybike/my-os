6a3cf7eb5b91f2cb8c6262fb4245f517064296ff
jsn_lee_ <leejunwoo99990@gmail.com>
2025-02-04 08:39:04 -0800

    note update

--- notes/1_z_code.rs
+++ notes/1_z_code.rs
@@ -15,25 +15,3 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
-//===========================================================
-//===========================================================
-
-static HELLO: &[u8] = b"Hello World!";
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;    // A raw pointer
-
-    // Iterate over the bytes of static "HELLO" byte string.
-    for (i, &byte) in HELLO.iter().enumerate() {
-        // unsafe because Rust compiler cant prove that raw pointers we create are valid
-        unsafe {
-            // uses offset to write the string bytes and corresponding color byte
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
-}
